2bec5fb5a7e56cd1f01f706f8fbc5e3322a8d808
1806PiYuSh1806 <piyushupadhyay37756@gmail.com>
2024-06-06 22:06:44 +0530

    connected backend with frontend

--- frontend/src/Hooks/useCart.js
+++ frontend/src/Hooks/useCart.js
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { sample_foods } from "../data";
 
 const CartContext = createContext(null);
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import CartProvider from "./Hooks/useCart";
+
+import './axiosConfig.js'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
